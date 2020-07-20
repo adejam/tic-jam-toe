5fd7bfdf427c3b6deed75ac57d7d9bebed6d66bb
adejam <oluwaloseyifunmiadeleye1@gmail.com>
2020-07-20 12:06:15 +0100

    feat: Add functionality to push player box to board

--- js/index.js
+++ js/index.js
@@ -3,14 +3,35 @@ const board = (playerOneBoard, playerTwoBoard) => ({
   playerTwoBoard,
 });
 
+const p1 = [];
+const p2 = [];
+const newBoard = board(p1, p2);
 const originalBoard = [];
 const playerOne = 'X';
 const playerTwo = 'O';
 const boxes = Array.from(document.querySelectorAll('.box'));
 const playGame = document.querySelector('#playGame');
 
-function playerTurn(id, player) {
+function checkForWin() {
   // body...
+}
+
+function playerTurn(id, player) {
+  if (boxes[id].innerText === '') {
+    if (player === playerOne) {
+      id = parseInt(id, 10);
+      newBoard.playerOneBoard.push(id);
+      originalBoard.push(id);
+      document.getElementById(id).innerText = player;
+      checkForWin(newBoard.playerOneBoard, player);
+    } else {
+      id = parseInt(id, 10);
+      newBoard.playerTwoBoard.push(id);
+      originalBoard.push(id);
+      document.getElementById(id).innerText = player;
+      checkForWin(newBoard.playerTwoBoard, player);
+    }
+  }
 }
 
 function whosTurn(box) {
