1af1335c4e8da144574cfb2a50015de6172878ed
adejam <oluwaloseyifunmiadeleye1@gmail.com>
2020-07-20 11:58:42 +0100

    feat: Add functionality to determine which player will pick a box

--- js/index.js
+++ js/index.js
@@ -3,17 +3,28 @@ const board = (playerOneBoard, playerTwoBoard) => ({
   playerTwoBoard,
 });
 
+const originalBoard = [];
+const playerOne = 'X';
+const playerTwo = 'O';
 const boxes = Array.from(document.querySelectorAll('.box'));
 const playGame = document.querySelector('#playGame');
 
-function whosTurn() {
+function playerTurn(id, player) {
   // body...
+}
+
+function whosTurn(box) {
+  if (originalBoard.length % 2 === 0) {
+    playerTurn(box.target.id, playerOne);
+  } else {
+    playerTurn(box.target.id, playerTwo);
+  }
 }
 
 function startGame() {
   document.querySelector('.statusPanel').style.display = 'none';
   for (let i = 0; i < boxes.length; i += 1) {
-    boxes[i].addEventListener('click', whosTurn, false);
+    boxes[i].addEventListener('click', whosTurn);
   }
 }
 
