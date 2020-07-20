4c88bc569d4bb060950182b3a18aaf5e8e366da6
adejam <oluwaloseyifunmiadeleye1@gmail.com>
2020-07-20 12:40:46 +0100

    feat: Add functionality to chech if a player has won or if d game ends in a tie

--- js/index.js
+++ js/index.js
@@ -11,9 +11,48 @@ const playerOne = 'X';
 const playerTwo = 'O';
 const boxes = Array.from(document.querySelectorAll('.box'));
 const playGame = document.querySelector('#playGame');
+const winAlgorithms = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2],
+];
+let playerWins = {};
+const checker = (playerBoard, winArray) => winArray.every(v => playerBoard.includes(v));
 
-function checkForWin() {
+function gameOver() {
   // body...
+}
+
+function checkForWin(playerBoard, player) {
+  if (originalBoard.length < 9) {
+    let winCount = 0;
+    for (let i = 0; i < winAlgorithms.length; i += 1) {
+      const winArray = winAlgorithms[i];
+      if (checker(playerBoard, winArray) === true) {
+        winCount += 1;
+        playerWins = { winArray, player };
+      }
+    }
+    if (winCount === 1) {
+      gameOver(playerWins);
+    } else {
+      for (let i = 0; i < boxes.length; i += 1) {
+        boxes[i].addEventListener('click', whosTurn);
+      }
+    }
+  } else {
+    for (let i = 0; i < boxes.length; i += 1) {
+      boxes[i].removeEventListener('click', whosTurn);
+      boxes[i].style.backgroundColor = '#ffc107';
+    }
+    document.querySelector('.statusPanel').style.display = 'block';
+    document.querySelector('.text').innerText = 'Its a Tie!';
+  }
 }
 
 function playerTurn(id, player) {
